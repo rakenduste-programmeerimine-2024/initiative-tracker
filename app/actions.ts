3be150542b52841e4fd42303bb7efaec8c335de7
Taiki-Viia Tungal <taikiviia@gmail.com>
2025-01-13 03:35:21 +0200

    fix: update import path for encodedRedirect to match filename casing

--- app/actions.ts
+++ app/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { encodedRedirect } from "@/utils/encodedRedirect"
+import { encodedRedirect } from "@/utils/encoded-redirect"
 import { createClient } from "@/utils/supabase/server"
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
